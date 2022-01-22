fdbd069aad35dacae386eb5d1709c3e3a5c06e4c
Eric <eburch6@student.gsu.edu>
2022-01-21 19:28:35 -0500

    Update to Number 1 and code for Number 2.

--- HW3.py
+++ HW3.py
@@ -1,18 +1,27 @@
+import math
+
+#Used help from w3schools and stackoverflow.
 def numLetterCheck():
-    word = "Test String"
+    word = input("Enter word to test.")
     vowel = 0 #vowel count
     consonant = 0 #consonant count
 
-    for letter in word:
+    for letter in word: #loops through all letters in the word
         if letter in ("aeiouAEIOU"):
             vowel += 1
         else:
             consonant += 1
-    if vowel > consonant:
-        return True
-    elif consonant > vowel:
-        return False
-    else:
+    if vowel == consonant:
         return None
+    else:
+        return vowel > consonant
+
+#Used python documentation for help.
+def cylinderVol():
+   height = float(input("Enter height of cylinder."))
+   radius = float(input("Enter radius of cylinder."))
+   volume = float(height * math.pow(radius, 2) * math.pi)
+   return volume
 
 print(numLetterCheck())
+print(cylinderVol())
