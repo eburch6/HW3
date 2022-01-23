f2c2c01c40ed54de2b3d71fca6f610869d69a430
Eric <eburch6@student.gsu.edu>
2022-01-22 23:47:45 -0500

    Numbers 3 and 4

--- HW3.py
+++ HW3.py
@@ -1,4 +1,6 @@
+from cgi import test
 import math
+from subprocess import list2cmdline
 
 #Used help from w3schools and stackoverflow.
 def numLetterCheck():
@@ -23,5 +25,27 @@ def cylinderVol():
    volume = float(height * math.pow(radius, 2) * math.pi)
    return volume
 
-print(numLetterCheck())
-print(cylinderVol())
+#Help from StackOverflow and W3schools
+def csvMaker(list):
+    tempStr = ', '.join(list)
+    return tempStr
+
+def csvList(listOfLists):
+    f = open("csv.txt", "w")
+    for i in listOfLists:
+      temp = csvMaker(i)
+      f.write(temp + "\n")
+    f.close()
+    f = open("csv.txt", "r")
+    return f.read()
+    
+
+list = ["Alligator", "Mask", "Gladiator", "Bellybutton", "Handle"]
+list1 = ["Crocodile", "Face", "Centurion"]
+list2 = ["Sheath", "Button", "Random"]
+list3 = ["Supercalifragilistic", "Nevada", "Las Vegas"]
+listOfLists = [list1, list2, list3]
+#print(numLetterCheck())
+#print(cylinderVol())
+#print(csvMaker(list))
+print(csvList(listOfLists))
