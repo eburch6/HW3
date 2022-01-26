a6c3c1184eeda914d4f7991fe4955b35c8363e75
Eric <eburch6@student.gsu.edu>
2022-01-25 20:09:41 -0500

    Problems 5-8 and changes to previous problems

--- HW3.py
+++ HW3.py
@@ -1,3 +1,4 @@
+from decimal import DivisionByZero
 import math
 import os
 
@@ -28,20 +29,53 @@ def csvMaker(lis):
 
 #Help from StackOverflow, W3Schools, and Python Docs
 def csvList(listOfLists):
-    f = open("././Software_Engineering/text.csv", "w")
+    f = open("././Software_Engineering/hw3/temp.csv", "w")
     for i in listOfLists:
         temp = csvMaker(i)
         f.write(temp + "\n")
     f.close()
-    return os.path.abspath('text.csv')
+    return os.path.abspath('temp.csv')
+
+#help from W3Schools
+def reverseCsvList(file):
+    lines = file.read()
+    listoflists = lines.split("\n")
+    file.close()
+    return listoflists
     
+def divisionErrorCatcher(num1, num2):
+    try:   
+        temp = num1 / num2
+        print("Answer=", temp, "Succesful Division.")
+    except(ZeroDivisionError):
+        print("Divide by zero error.")
+    
+#help from W3Schools
+def duplicateRemover(listOfNums):
+    temp = []
+    for i in listOfNums:
+        if i not in temp:
+            temp.append(i)
+    return temp
+
+def directoryCreator():
+    directory = "hw3-folder"
+    parent = "/home/eburch6/Software_Engineering/hw3"
+    path = os.path.join(parent, directory)
+    os.mkdir(path)
 
 lis = ["Alligator", "Mask", "Gladiator", "Bellybutton", "Handle"]
 list1 = ["Crocodile", "Face", "Centurion"]
 list2 = ["Sheath", "Button", "Random"]
 list3 = ["Supercalifragilistic", "Nevada", "Las Vegas"]
 listOfLists = [list1, list2, list3]
+listOfNums = [1,5,3,5,2,1,5,9,8,7,6,1,0]
+f = open("././Software_Engineering/hw3/temp.csv", "r")
 print(numLetterCheck("Word"))
 print(cylinderVol(5, 5))
 print(csvMaker(lis))
 print(csvList(listOfLists))
+print(reverseCsvList(f))
+divisionErrorCatcher(5,0)
+print(duplicateRemover(listOfNums))
+directoryCreator()
